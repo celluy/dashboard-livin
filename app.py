9254f209945103f819ee3eb0458583725954fec2
celluy <gitamarchella6@gmail.com>
2026-07-06 09:42:32 +0700

    p

--- app.py
+++ app.py
@@ -1,17 +1,1 @@
-import streamlit as st
-import pandas as pd
 
-st.title("Dashboard Livin by Mandiri")
-
-survey = pd.read_csv("survey_clean.csv")
-
-gender = st.selectbox(
-    "Pilih Gender",
-    survey["Jenis Kelamin"].unique()
-)
-
-filtered = survey[
-    survey["Jenis Kelamin"] == gender
-]
-
-st.dataframe(filtered)
